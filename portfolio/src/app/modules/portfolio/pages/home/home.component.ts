df3cc7c3d21d42a2aaee1e03dfd7fbdcdbcd83df
Renan Caetano da Silva <126742354+renancae91@users.noreply.github.com>
2024-03-02 22:19:48 -0300

    Update home.component.ts

--- portfolio/src/app/modules/portfolio/pages/home/home.component.ts
+++ portfolio/src/app/modules/portfolio/pages/home/home.component.ts
@@ -9,7 +9,7 @@ import { ProjectsComponent } from '../../components/projects/projects.component'
   standalone: true,
   imports: [HeaderComponent, KnowledgeComponent, ExperiencesComponent, ProjectsComponent],
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.scss']
+  styleUrl: './home.component.scss'
 })
 export class HomeComponent {
 
